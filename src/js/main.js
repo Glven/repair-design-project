07152b28863b3856c2bc42d06942c968059db54d
Tigran Oganesyan <t-oganesyan00@yandex.ru>
2020-05-28 18:00:07 +0300

    fix: trying increase page speed

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,11 @@ $(document).ready(function () {
   var headerHeight = $('.header').height();
   var send = "<div class='sended'> <div class='sended__element'> Спасибо за отправленную заявку <div class='sended__element'>Вы также можете вступить в нашу группу в <a href='http://vk.com' class='sended__item'>ВКонтакте</a></div></div>";
 
+  $result_html = preg_replace(
+    ['/ {2,}/', '/<!--.*?-->|\t|(?:\r?\n[ \t]*)+/s'],
+    [' ', ''],
+    $result_html
+  );
 
   wow = new WOW(
     {
